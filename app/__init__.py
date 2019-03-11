5350e1e4957b2982b8487f186b1fb7e5c5d3811f
Paul Kruger <paul@miamiphp.org>
2019-03-11 12:06:10 -0700

    Adding moment js for proper timestamps

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
@@ -18,6 +19,8 @@ login.login_view = 'login'
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
