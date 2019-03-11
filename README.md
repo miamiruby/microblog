c6e8188d5319b43ddabec1c78db7d7212393f5ae
Paul Kruger <paul@miamiphp.org>
2019-03-10 17:33:31 -0700

    adding password reset email and threading

--- README.md
+++ README.md
@@ -8,10 +8,26 @@ flask db migrate -m "new fields in user model"
 flask db upgrade
 
 # setup fake email server in debug
+MAIL_SERVER=localhost
+MAIL_PORT=8025
+
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=<your-gmail-username>
+MAIL_PASSWORD=<your-gmail-password>
+
 python -m smtpd -n -c DebuggingServer localhost:8025
 
-export MAIL_SERVER=smtp.googlemail.com
-export MAIL_PORT=587
-export MAIL_USE_TLS=1
-export MAIL_USERNAME=<your-gmail-username>
-export MAIL_PASSWORD=<your-gmail-password>
+# sending mail
+>>> from flask_mail import Message
+>>> from app import mail
+>>> msg = Message('test subject', sender=app.config['ADMINS'][0],recipients=['your-email@example.com'])
+>>> msg.body = 'text body'
+>>> msg.html = '<h1>HTML body</h1>'
+>>> mail.send(msg)
+
+# generate requirements.txt
+pip freeze > requirements.txt
+pip freeze > requirements-dev.txt
+
--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from datetime import datetime
+from time import time
+import jwt
+
+from app import app
 from app import db
 from app import login
+
 from flask_login import UserMixin
 from hashlib import md5
 
@@ -53,12 +58,28 @@ class User(UserMixin, db.Model):
     def is_following(self, user):
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
+
     def followed_posts(self):
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
